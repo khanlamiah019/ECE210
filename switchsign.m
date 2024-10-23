f90b9f45f5ad869dc7b322115a4894630a646940
khanlamiah019 <khanlamiah017@gmail.com>
2024-10-23 15:57:45 -0400

    Delete switchsign.m

--- switchsign.m
+++
@@ -1,9 +0,0 @@
-%% functions for my sinc is broken 
-% Lamiah Khan February 17,2024
-% (2) 
-
-function [diffSign] = switchsign(x)
-    xSign = sign(x);
-    signChange = [false, diff(xSign) ~= 0];
-    diffSign = logical(signChange);
-end
