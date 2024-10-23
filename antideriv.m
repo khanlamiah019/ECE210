73fbdf6e252a457e2234fe8ce05b5e310e82f118
khanlamiah019 <khanlamiah017@gmail.com>
2024-10-23 15:55:20 -0400

    Delete antideriv.m

--- antideriv.m
+++
@@ -1,9 +0,0 @@
-%% functions for my sinc is broken 
-% Lamiah Khan February 17,2024
-% (1) 
-
-function [diffEq] = antideriv(y, x)
-    dff = diff(x);
-    dfdf = dff(1);
-    diffEq = cumtrapz(y)*dfdf; 
-end
